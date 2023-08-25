6fcae3d960601e3e2425d148dddd93c1eac1e1a0
Ayush Raj <rajayush1412@gmail.com>
2023-08-25 20:42:25 +0530

    Task management sytem

---
+++ Unit 1/findSmallestMissing.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+int findSmallestMissing(int arr[], int size, int x) {
+    int left = 0, right = size - 1;
+    int result = -1;  
+    
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] > x && (mid == 0 || arr[mid - 1] > arr[mid] - 1)) {
+            result = arr[mid] - 1;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    
+    if (result == -1) {
+        result = x + 1;
+    }
+    
+    return result;
+}
+int main() {
+    int size;
+    std::cin >> size;
+
+    int arr[size];
+    for (int i = 0; i < size; ++i) {
+        std::cin >> arr[i];
+    }
+
+    int x;
+    std::cin >> x;
+
+    int smallestMissing = findSmallestMissing(arr, size, x);
+    std::cout << "The smallest missing positive integer greater than " << x << " is: " << smallestMissing << std::endl;
+
+    return 0;
+}
