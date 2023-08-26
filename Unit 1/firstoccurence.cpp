230cefb7fbd70ce4b1f39aad6e0652ec5cdfc957
Ayush Raj <rajayush1412@gmail.com>
2023-08-26 16:19:08 +0530

    first occurrence and last occurrence

---
+++ Unit 1/firstoccurence.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+int firstoccurence(const int prices[], int size, int target) {
+    int left = 0, right = size - 1, result = -1;
+    
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        
+        if (prices[mid] == target) {
+            result = mid;
+            right = mid - 1; 
+        } else if (prices[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    
+    return result;
+}
+
+int findLastOccurrence(const int prices[], int size, int target) {
+    int left = 0, right = size - 1, result = -1;
+    
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        
+        if (prices[mid] == target) {
+            result = mid;
+            left = mid + 1;
+        } else if (prices[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    
+    return result;
+}
+
+int main() {
+    int size;
+    cin >> size;
+
+    int *prices = new int[size];
+    for (int i = 0; i < size; ++i) {
+        cin >> prices[i];
+    }
+
+    int target;
+    cin >> target;
+    
+    int firstOccurrence = firstoccurence(prices, size, target);
+    int lastOccurrence = findLastOccurrence(prices, size, target);
+    
+    if (firstOccurrence != -1 && lastOccurrence != -1) {
+        cout << "The first occurrence of " << target << " is at index " << firstOccurrence << "." << endl;
+        cout << "The last occurrence of " << target << " is at index " << lastOccurrence << "." << endl;
+    } else {
+        cout << "Target value " << target << " not found in the array." << endl;
+    }
+    
+    delete[] prices;
+
+    return 0;
+}
