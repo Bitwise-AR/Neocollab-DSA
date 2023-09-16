cdd36db9effcc909d14cf7b4b9ff8a5eff688967
Ayush Raj <rajayush1412@gmail.com>
2023-09-16 13:55:56 +0530

    infix to postfix

---
+++ Unit 3/Nithish.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int precedence(char op) {
+    if (op == '+' || op == '-')
+        return 1;
+    if (op == '*' || op == '/')
+        return 2;
+    return 0;
+}
+
+string convert(string infix) {
+    stack<char> operators;
+    string postfix = "";
+
+    for (char c : infix) {
+        if (isdigit(c)) {
+            postfix += c;
+        } else if (c == '(') {
+            operators.push(c);
+        } else if (c == ')') {
+            while (!operators.empty() && operators.top() != '(') {
+                postfix += operators.top();
+                operators.pop();
+            }
+            if (!operators.empty() && operators.top() == '(') {
+                operators.pop();
+            }
+        } else {
+            while (!operators.empty() && precedence(c) <= precedence(operators.top())) {
+                postfix += operators.top();
+                operators.pop();
+            }
+            operators.push(c);
+        }
+    }
+
+    while (!operators.empty()) {
+        postfix += operators.top();
+        operators.pop();
+    }
+
+    return postfix;
+}
+
+int main() {
+    string infixexpr;
+    cin >> infixexpr;
+
+    string postfixexpr = convert(infixexpr);
+
+    cout << "Postfix expression: " << postfixexpr << endl;
+
+    return 0;
+}
