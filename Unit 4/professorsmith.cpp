ba0d4eb2ca6ca975cfc9621624f77b9de6218c06
Ayush Raj <rajayush1412@gmail.com>
2023-09-26 20:31:50 +0530

    student attending a class on sorting techniques

---
+++ Unit 4/professorsmith.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+void merge(vector<double>& arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    
+    vector<double> leftArr(n1);
+    vector<double> rightArr(n2);
+    
+    for (int i = 0; i < n1; i++) {
+        leftArr[i] = arr[left + i];
+    }
+    for (int i = 0; i < n2; i++) {
+        rightArr[i] = arr[mid + 1 + i];
+    }
+    
+    int i = 0, j = 0, k = left;
+    
+    while (i < n1 && j < n2) {
+        if (leftArr[i] <= rightArr[j]) {
+            arr[k++] = leftArr[i++];
+        }
+        else {
+            arr[k++] = rightArr[j++];
+        }
+    }
+    
+    while (i < n1) {
+        arr[k++] = leftArr[i++];
+    }
+    
+    while (j < n2) {
+        arr[k++] = rightArr[j++];
+    }
+}
+
+void mergeSort(vector<double>& arr, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    
+    vector<double> arr(n);
+    
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    mergeSort(arr, 0, n - 1);
+    
+    cout << "Sorted Array:" << endl;
+    
+    cout << fixed << setprecision(2);
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    
+    cout << endl;
+    
+    return 0;
+}
